d1c5de44ad181586613f4c56ab9354ba4cb8fb5e
Peter Stromquist <peter@twistedstream.com>
2013-04-19 13:18:16 -0500

    Create new package for Grails

---
+++ Grails/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'Grails'
+$grailsVersionName = 'grails-2.2.1'
+$url = 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/{0}.zip' -f $grailsVersionName
+$unzipLocation = $Env:chocolatey_bin_root
+if (!$unzipLocation) {
+	$unzipLocation = 'C:\grails'
+}
+$homeDirectory = Join-Path $unzipLocation $grailsVersionName
+
+try { 
+
+  Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLocation"
+
+  Install-ChocolateyEnvironmentVariable "GRAILS_HOME" $homeDirectory Machine
+  Install-ChocolateyPath "%JAVA_HOME%\bin" Machine
+  Install-ChocolateyPath "%GRAILS_HOME%\bin" Machine
+  
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
