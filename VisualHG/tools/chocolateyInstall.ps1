2d4c59280f4d2b36946447d2922a174d72539dab
Peter Stromquist <peter@twistedstream.com>
2014-02-04 06:46:01 +0000

    Upgrade VisualHG to latest version

--- VisualHG/tools/chocolateyInstall.ps1
+++ VisualHG/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'VisualHG'
 $installerType = 'MSI'
-$url = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=visualhg&DownloadId=289940&FileTime=129623969865630000&Build=20218'
+$url = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=visualhg&DownloadId=763804&FileTime=130304700673700000&Build=20865'
 $url64 = $url
 $silentArgs = '/quiet'
 $validExitCodes = @(0,3010)
